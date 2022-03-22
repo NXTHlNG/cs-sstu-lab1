c4877e3074d58a30c73915c5fe15a88bff60c51f
NXTHING <nxthing.az@gmail.com>
2022-03-22 22:52:37 +0400

    added solution for 2nd task

---
+++ Lab1/Controllers/InputController.cs
@@ -0,0 +1,20 @@
+﻿using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class InputController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(InputViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Lab1/Models/InputViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+    public class InputViewModel
+    {
+        public string Text { get; set; }
+
+        public IEnumerable<string> Values { get; set; }
+        
+        public IEnumerable<string> Values1 { get; set; }
+
+        public string Option { get; set; }
+       
+    }
+}
