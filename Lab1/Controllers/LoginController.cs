3a6681459f0437112b802f40fd268a2fcf8a6ee9
NXTHING <nxthing.az@gmail.com>
2022-03-22 21:14:47 +0400

    Добавьте файлы проекта.

---
+++ Lab1/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab1.Models;
+using System.Collections.Generic;
+
+namespace Lab1.Controllers
+{
+    public class LoginController : Controller
+    {
+        private static IDictionary<string, string> dictionary = new Dictionary<string, string>();
+
+        static LoginController()
+        {
+            dictionary.Add("admin", "admin");
+            dictionary.Add("root", "root");
+            dictionary.Add("qwerty", "123456");
+            dictionary.Add("123456", "qwerty");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn(LoginViewModel model)
+        {
+            if (model.Login == null || model.Password == null)
+            {
+                return View("InvalidSignIn");
+            }
+
+            string confrimPass;
+
+            try
+            {
+                confrimPass = dictionary[model.Login];
+            }
+            catch (KeyNotFoundException e)
+            {
+                return View("InvalidSignIn");
+            }
+
+            if (confrimPass != null && confrimPass == model.Password)
+            {
+                return View(model);
+            }
+
+            return View("InvalidSignIn");
+        }
+    }
+}
